9eb983f7dd8b9dc47e6a58ed708f14b217a91f27
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-04-07 16:03:29 -0500

    java variables

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,11 @@
 public class Hello {
 
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello Eleanor");
+
+        int myFirstNumber = (10 + 5) + (2 * 3) ;
+        System.out.println(myFirstNumber);
+
     }
 
 
