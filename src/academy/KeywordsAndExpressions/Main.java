68ac95b3ed0556c9bbf9234b4a83dbd99c464d0e
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-04-14 14:11:35 -0500

    Keywords and expressions

---
+++ src/academy/KeywordsAndExpressions/Main.java
@@ -0,0 +1,20 @@
+package academy.KeywordsAndExpressions;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        double kilometres = (100 * 1.609334);
+        int highScore = 50;
+        if(highScore == 50){
+            System.out.println("This is an expression");
+        }
+
+        int score = 100;
+        if (score > 99){
+            System.out.println("You got the high score!");
+            score = 0;
+        }
+
+    }
+}
