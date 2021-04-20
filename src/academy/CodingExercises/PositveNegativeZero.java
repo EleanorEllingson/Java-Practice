7fbe353d2fbb85f29c93b4c3b92ab0153e346ed2
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-04-20 14:10:54 -0500

    Coding exercise practice

---
+++ src/academy/CodingExercises/PositveNegativeZero.java
@@ -0,0 +1,20 @@
+package academy.CodingExercises;
+
+public class PositveNegativeZero {
+
+    public static void main(String[] args) {
+        checkNumber(5);
+        checkNumber(-10);
+        checkNumber(0);
+    }
+
+    public static void checkNumber(int number){
+        if(number > 0){
+            System.out.println("positive");
+        }else if(number < 0){
+            System.out.println("negative");
+        }else if(number == 0){
+            System.out.println("zero");
+        }
+    }
+}
