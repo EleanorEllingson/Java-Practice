fa699f50b883849f171b506c2652b7ed3a78d399
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-04-29 16:05:31 -0500

    feet and inches

---
+++ src/academy/CodingExercises/MethodOverloading/FeetAndInches.java
@@ -0,0 +1,31 @@
+package academy.CodingExercises.MethodOverloading;
+
+public class FeetAndInches {
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches){
+        double feetInches = feet * 12;
+        double centimeters = (inches + feetInches) * 2.54;
+        if(feet < 0){
+            return -1;
+        }else if(inches < 0 || inches > 12){
+            return -1;
+        }else{
+            System.out.println(centimeters);
+        }
+
+        return centimeters;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches){
+
+
+         if(inches < 0){
+            return -1;
+        }
+
+        double feet = (int) inches / 12;
+         double remainingInches = (int) inches % 12;
+        System.out.println(inches + " inches is equal to " + feet + remainingInches + " inches");
+        return calcFeetAndInchesToCentimeters(feet, inches);
+    }
+
+}
---
+++ src/academy/CodingExercises/MethodOverloading/Main.java
@@ -0,0 +1,8 @@
+package academy.CodingExercises.MethodOverloading;
+
+public class Main {
+    public static void main(String[] args) {
+        FeetAndInches.calcFeetAndInchesToCentimeters(4,6);
+        FeetAndInches.calcFeetAndInchesToCentimeters(100);
+    }
+}
