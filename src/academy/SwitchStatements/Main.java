03441e71e82b7cf2e4caefc349feee3912e9a680
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-05-10 14:58:03 -0500

    Switch statement value

---
+++ src/academy/SwitchStatements/Main.java
@@ -0,0 +1,27 @@
+package academy.SwitchStatements;
+
+public class Main {
+    public static void main(String[] args){
+//        int value = 3;
+//        if(value == 1){
+//            System.out.println("Value was 1");
+//        }else if(value == 2){
+//            System.out.println("Value was 2");
+//        }else{
+//            System.out.println("Was not 1 or 2");
+//        }
+
+        int switchValue = 3;
+        switch(switchValue){
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
+        }
+    }
+}
