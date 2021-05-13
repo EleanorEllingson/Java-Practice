1f7f79fbda3eeb7a0439024eacc04f773cb1040d
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-05-13 13:26:31 -0500

    started Number of days in month

---
+++ src/academy/CodingExercises/Switch/NumberOfDaysInMonth.java
@@ -0,0 +1,12 @@
+package academy.CodingExercises.Switch;
+
+public class NumberOfDaysInMonth {
+    public static boolean isLeapYear(int year){
+        boolean leapYear = true;
+        if(year < 1 || year > 9999){
+            leapYear = false;
+        }else if(
+
+        )
+    }
+}
