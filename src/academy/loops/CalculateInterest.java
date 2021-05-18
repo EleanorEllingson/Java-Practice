67b179223d7d36ec4c3f92b47045c7211ef242e1
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-05-17 22:18:56 -0500

    for loop for prime numbers

--- src/academy/loops/CalculateInterest.java
+++ src/academy/loops/CalculateInterest.java
@@ -5,17 +5,39 @@ public class CalculateInterest {
 //        for(int i =0; i < 5; i++){
 //            System.out.println(("Loop " + i + " hello!"));
 //        }
-        for(int i = 2; i < 9; i++){
-            System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
-        }
-        for(int i = 8; i > 1; i--){
-            System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
+//        for (int i = 2; i < 9; i++) {
+//            System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
+//        }
+//        for (int i = 8; i > 1; i--) {
+//            System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
+//        }
+        int count = 0;
+        for (int i = 20; i < 50; i++){
+            if(isPrime(i)){
+                System.out.println(i);
+                count++;
+                    if(count >= 3){
+                        break;
+                    }
+            }
         }
 
     }
-    public static double calculateInterest(double amount, double interestRate){
 
-        return(amount * (interestRate/100));
+    public static double calculateInterest(double amount, double interestRate) {
+
+        return (amount * (interestRate / 100));
     }
 
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
