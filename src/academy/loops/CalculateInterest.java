99e1867c60eb24e35ddd7ed709e43c46fde10191
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-05-17 21:55:49 -0500

    interest for loop

--- src/academy/loops/CalculateInterest.java
+++ src/academy/loops/CalculateInterest.java
@@ -2,9 +2,17 @@ package academy.loops;
 
 public class CalculateInterest {
     public static void main(String[] args) {
-        for(int i =0; i < 5; i++){
-            System.out.println(("Loop " + i + " hello!"));
+//        for(int i =0; i < 5; i++){
+//            System.out.println(("Loop " + i + " hello!"));
+//        }
+        for(int i = 2; i < 9; i++){
+            System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
         }
+
+    }
+    public static double calculateInterest(double amount, double interestRate){
+
+        return(amount * (interestRate/100));
     }
 
 }
