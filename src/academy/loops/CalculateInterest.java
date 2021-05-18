993619af1a3e0d8d0791ffbaac5e7beffa3fa762
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-05-17 21:18:46 -0500

    Started loops

---
+++ src/academy/loops/CalculateInterest.java
@@ -0,0 +1,10 @@
+package academy.loops;
+
+public class CalculateInterest {
+    public static void main(String[] args) {
+        for(int i =0; i < 5; i++){
+            System.out.println(("Loop " + i + " hello!"));
+        }
+    }
+
+}
