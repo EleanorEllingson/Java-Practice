ee3972a1e2c8d6e48005db8ecccc724a689336fc
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-05-19 22:33:03 -0500

    isOdd

---
+++ src/academy/CodingExercises/Loops/SumOddRange.java
@@ -0,0 +1,16 @@
+package academy.CodingExercises.Loops;
+
+public class SumOddRange {
+
+    public static boolean isOdd(int number){
+        if(number <= 0){
+            return false;
+        }else if(number % 2 != 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+
+}
