23d508fb979e9e496242ffeea76692919950e2ba
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-05-31 21:35:19 -0500

    Finished FactorPrinter

--- src/academy/CodingExercises/Loops/FactorPrinter.java
+++ src/academy/CodingExercises/Loops/FactorPrinter.java
@@ -5,5 +5,14 @@ public class FactorPrinter {
         if(number < 1){
             System.out.println("Invalid Value");
         }
+        int factor = 1;
+        while(factor <= number){
+            if(number % factor == 0){
+                System.out.println(factor);
+                factor++;
+            }else{
+                factor++;
+            }
+        }
     }
 }
--- src/academy/CodingExercises/Loops/Main.java
+++ src/academy/CodingExercises/Loops/Main.java
@@ -2,14 +2,15 @@ package academy.CodingExercises.Loops;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(DigitSumChallenge.sumDigits(777777777));
-        System.out.println(NumberPalindrome.isPalindrome(4114));
-        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(5));
-        System.out.println(EvenDigitSum.getEvenDigitSum(324746));
-        System.out.println(SharedDigit.hasSharedDigit(10, 2));
-        System.out.println(LastDigitChecker.hasSameLastDigit(50, 561, 481));
-        System.out.println(LastDigitChecker.isValid(50));
-        System.out.println("GreatestCommonDivisor " + GreatestCommonDivisor.getGreatestCommonDivisor(100, 60));
+//        System.out.println(DigitSumChallenge.sumDigits(777777777));
+//        System.out.println(NumberPalindrome.isPalindrome(4114));
+//        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(5));
+//        System.out.println(EvenDigitSum.getEvenDigitSum(324746));
+//        System.out.println(SharedDigit.hasSharedDigit(10, 2));
+//        System.out.println(LastDigitChecker.hasSameLastDigit(50, 561, 481));
+//        System.out.println(LastDigitChecker.isValid(50));
+//        System.out.println("GreatestCommonDivisor " + GreatestCommonDivisor.getGreatestCommonDivisor(100, 60));
+    FactorPrinter.printFactors(-1);
 
     }
 }
