401822623eef7a56baf75305a74f5c3343508e13
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-05-31 21:22:57 -0500

    Started FactorPrinter

---
+++ src/academy/CodingExercises/Loops/FactorPrinter.java
@@ -0,0 +1,9 @@
+package academy.CodingExercises.Loops;
+
+public class FactorPrinter {
+    public static void printFactors(int number){
+        if(number < 1){
+            System.out.println("Invalid Value");
+        }
+    }
+}
