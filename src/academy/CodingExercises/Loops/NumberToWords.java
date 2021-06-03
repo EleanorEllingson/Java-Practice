ea9aec860d1e65ff098c7b9173522c4b0c786781
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-06-02 23:55:14 -0500

    finished NumberToWords

--- src/academy/CodingExercises/Loops/NumberToWords.java
+++ src/academy/CodingExercises/Loops/NumberToWords.java
@@ -5,46 +5,59 @@ public class NumberToWords {
         if(number < 0){
             System.out.println("Invalid Value");
         }
-
+        if(number == 0){
+            System.out.println("Zero");
+        }
+        int revNumber = reverse(number);
         int newNum = reverse(number);
+
         int oneNum;
         while (newNum > 0){
             oneNum = newNum % 10;
             switch (oneNum){
                 case 0:
-                    System.out.println("ZERO");
+                    System.out.println("Zero");
                     break;
                 case 1:
-                    System.out.println("ONE");
+                    System.out.println("One");
                     break;
                 case 2:
-                    System.out.println("TWO");
+                    System.out.println("Two");
                     break;
                 case 3:
-                    System.out.println("THREE");
+                    System.out.println("Three");
                     break;
                 case 4:
-                    System.out.println("FOUR");
+                    System.out.println("Four");
                     break;
                 case 5:
-                    System.out.println("FIVE");
+                    System.out.println("Five");
                     break;
                 case 6:
-                    System.out.println("SIX");
+                    System.out.println("Six");
                     break;
                 case 7:
-                    System.out.println("SEVEN");
+                    System.out.println("Seven");
                     break;
                 case 8:
-                    System.out.println("EIGHT");
+                    System.out.println("Eight");
                     break;
                 case 9:
-                    System.out.println("NINE");
+                    System.out.println("Nine");
                     break;
                 default:
-                    System.out.println("OTHER");
+                    System.out.println("Other");
             }
+
             newNum = newNum / 10;
+
+        }
+        int zeroDifference = getDigitCount(number) - getDigitCount(revNumber);
+
+        if(zeroDifference != 0){
+            for(int i = 1; i <= zeroDifference; i++){
+                System.out.println("Zero");
+            }
         }
     }
 
@@ -58,6 +71,21 @@ public class NumberToWords {
             num /= 10;
         }
         return reverse;
+    }
+    public static int getDigitCount(int number){
+        if(number < 0){
+            return -1;
+        }
+        if(number == 0){
+            return 1;
+        }
+        int newNum = number;
+        int count = 0;
+        while (newNum > 0) {
+            newNum = newNum / 10;
+            count++;
+        }
+        return count;
     }
 
 }
