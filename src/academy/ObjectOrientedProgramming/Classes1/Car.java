ea4daf85fb5b337cfd600784e10a16fc199b4b4c
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-06-09 13:29:06 -0500

    First part of classes

---
+++ src/academy/ObjectOrientedProgramming/Classes1/Car.java
@@ -0,0 +1,48 @@
+package academy.ObjectOrientedProgramming.Classes1;
+
+import java.util.Locale;
+
+public class Car {
+    private int doors;
+    private int wheels;
+    private String model;
+    private String color;
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if(validModel.equals("carrera") || validModel.equals("commodore")){
+            this.model = model;
+        }else {
+            this.model = "Unknown";
+        }
+
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
