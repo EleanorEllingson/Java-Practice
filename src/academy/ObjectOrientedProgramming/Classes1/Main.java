221510709e4ba848118233a40ebcb63f76096982
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-06-11 13:31:14 -0500

    SimpleCalculator

--- src/academy/ObjectOrientedProgramming/Classes1/Main.java
+++ src/academy/ObjectOrientedProgramming/Classes1/Main.java
@@ -2,15 +2,15 @@ package academy.ObjectOrientedProgramming.Classes1;
 
 public class Main {
     public static void main(String[] args) {
-        Car porsche = new Car();
-        Car holden = new Car();
-        porsche.setModel("911");
-        System.out.println("Model is " + porsche.getModel());
-
-BankAccount bobsAccount = new BankAccount();
-bobsAccount.withdrawal(100);
-bobsAccount.depositFunds(546);
-bobsAccount.withdrawal(340);
+//        Car porsche = new Car();
+//        Car holden = new Car();
+//        porsche.setModel("911");
+//        System.out.println("Model is " + porsche.getModel());
+//
+//BankAccount bobsAccount = new BankAccount();
+//bobsAccount.withdrawal(100);
+//bobsAccount.depositFunds(546);
+//bobsAccount.withdrawal(340);
 
 
 
