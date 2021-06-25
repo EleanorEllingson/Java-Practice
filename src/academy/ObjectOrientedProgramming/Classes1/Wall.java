3fd526474cad0d0b092e55630c350e8a9a58a971
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-06-24 23:08:25 -0500

    Started Wall Class

---
+++ src/academy/ObjectOrientedProgramming/Classes1/Wall.java
@@ -0,0 +1,15 @@
+package academy.ObjectOrientedProgramming.Classes1;
+
+public class Wall {
+    private double width;
+    private double height;
+
+    public Wall() {
+
+    }
+
+    public Wall(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+}
